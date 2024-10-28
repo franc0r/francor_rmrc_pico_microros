8be18d71cc259c8793b2c1aed81ebdd7a461494e
Martin Bauernschmitt <martin.bauernschmitt@posteo.de>
2024-10-28 07:01:04 +0000

    Bugfix and changed devcontainer to docker hub container

--- include/main.h
+++ include/main.h
@@ -27,7 +27,6 @@
 #include "pico/stdlib.h"
 #include "pico/bootrom.h"
 #include "hardware/gpio.h"
-#include "hardware/adc.h"
 
 // Defines --------------------------------------------------------------------
 
